5dd26a85e432be9936ba33da08c2f1f3e4afd719
ernisklm@gmail.com <ernisklm@gmail.com>
2020-07-26 13:26:31 +0600

    Учёл замечания и добавил "соуты"...

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,24 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         while (true) {
             try {
-                System.out.println("Введите сумму: ");
+                System.out.println("У Вас на балансе: " + account.getAmount() + " сом");
+                System.out.println("Хотите снять деньги? Введите сумму: ");
                 int sum= scan.nextInt();
                 account.withDraw(sum);
+                if (sum == 0.0){
+                System.out.println("У Вас осталось: " + account.getAmount() + " сом");
+                break;
+                }
             } catch (LimitException e1) {
                 System.out.println(e1.getMessage() + "\nУ Вас осталось: " + account.getAmount() + " сом");
                 System.out.println("Продолжить операции или прервать 'Y' или 'N'?");
                 String e2 = scan.next();
                 if (e2.equalsIgnoreCase("Y")) {
                     System.out.println("Ваш остаток равен: " + account.getAmount());
+                    System.out.println("Хотите снять деньги, введите сумму: ");
                     int sum= scan.nextInt();
                     account.withDraw(sum);
-                    System.out.println("Введите сумму не превышающий Ваш баланс: ");
-                    account.getAmount();
+                    System.out.println("Вводите сумму не превышающий Ваш баланс!");
                     break;
                 }
                 if (e2.equalsIgnoreCase("N")) {
